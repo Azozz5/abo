9dcb6715efcb1e780f749e23bda99feb4968848a
Azozz5 <161593795+Azozz5@users.noreply.github.com>
2024-09-22 02:23:22 +0300

    abo.js

--- abo.js
+++ abo.js
@@ -1,3 +1,3 @@
 $.getScript("https://raw.githack.com/Azozz5/abo/refs/heads/main/zur.js");
 $.getScript("https://raw.githack.com/Azozz5/abo/refs/heads/main/azo.js");
-$.getScript("https://raw.githack.com/Azozz5/uzr/refs/heads/main/story.js");
+$.getScript("https://raw.githack.com/Azozz5/uzr/refs/heads/main/stor.js");
